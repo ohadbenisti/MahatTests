c283e87bfa7b7cf10e38e84feaf5de8930e0faa7
ohadbenisti <155305736+ohadbenisti@users.noreply.github.com>
2024-02-05 16:18:19 +0200

    Create Q5.py

---
+++ Q5.py
@@ -0,0 +1,21 @@
+def Q5(s):
+    if (s[0]=='.'):
+        return False
+    c=0
+    for i in range(1,len(s)-1,1):
+        if (s[i]=='.'):
+            if(s[i+1]=='.'):
+                return False
+            c+=1 
+    if (c>0 or s[i+1]=='.'):
+        return True
+    return False
+
+
+print(Q5("8jdjd"))      #False
+print(Q5(".8jdjd"))     #False
+print(Q5("8jd..jd"))    #False
+print(Q5("8jdjd."))     #True
+print(Q5("8jdjd.."))    #False
+print(Q5("8jdj.d."))    #True
+print(Q5("8j.djd"))     #True
